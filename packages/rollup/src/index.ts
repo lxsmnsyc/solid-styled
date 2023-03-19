884b94cfa11f088159bb74db13cbb7d559757ae1
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-19 16:02:28 +0800

    Fix sourcemap issues

--- packages/rollup/src/index.ts
+++ packages/rollup/src/index.ts
@@ -3,7 +3,6 @@ import { Plugin } from 'rollup';
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import * as babel from '@babel/core';
 import path from 'path';
-import ts from '@babel/preset-typescript';
 
 export interface SolidStyledPluginFilter {
   include?: FilterPattern;
@@ -31,26 +30,38 @@ export default function solidStyledPlugin(
     name: 'solid-styled',
     async transform(code, id) {
       if (filter(id)) {
+        const plugins: NonNullable<NonNullable<babel.TransformOptions['parserOpts']>['plugins']> = ['jsx'];
+        if (/\.[mc]tsx?$/i.test(id)) {
+          plugins.push('typescript');
+        }
         const result = await babel.transformAsync(code, {
           ...options.babel,
-          presets: [
-            [ts],
-            ...(options.babel?.presets ?? []),
-          ],
           plugins: [
             [solidStyledBabel, {
               verbose: options.verbose,
               prefix: options.prefix,
               source: id,
             }],
-            ...(options.babel?.plugins ?? []),
+            ...(options.babel?.plugins || []),
           ],
+          parserOpts: {
+            ...(options.babel?.parserOpts || {}),
+            plugins: [
+              ...(options.babel?.parserOpts?.plugins || []),
+              ...plugins,
+            ],
+          },
           filename: path.basename(id),
+          ast: false,
+          sourceMaps: true,
+          configFile: false,
+          babelrc: false,
+          sourceFileName: id,
         });
 
         if (result) {
           return {
-            code: result.code ?? '',
+            code: result.code || '',
             map: result.map,
           };
         }
--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -3,7 +3,6 @@ import { Plugin } from 'vite';
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import * as babel from '@babel/core';
 import path from 'path';
-import ts from '@babel/preset-typescript';
 
 export interface SolidStyledPluginFilter {
   include?: FilterPattern;
@@ -64,26 +63,38 @@ export default function solidStyledPlugin(
     },
     async transform(code, id) {
       if (filter(id)) {
+        const plugins: NonNullable<NonNullable<babel.TransformOptions['parserOpts']>['plugins']> = ['jsx'];
+        if (/\.[mc]tsx?$/i.test(id)) {
+          plugins.push('typescript');
+        }
         const result = await babel.transformAsync(code, {
           ...options.babel,
-          presets: [
-            [ts],
-            ...(options.babel?.presets ?? []),
-          ],
           plugins: [
             [solidStyledBabel, {
               verbose: isDev,
               prefix: options.prefix,
               source: id,
             }],
-            ...(options.babel?.plugins ?? []),
+            ...(options.babel?.plugins || []),
           ],
+          parserOpts: {
+            ...(options.babel?.parserOpts || {}),
+            plugins: [
+              ...(options.babel?.parserOpts?.plugins || []),
+              ...plugins,
+            ],
+          },
           filename: path.basename(id),
+          ast: false,
+          sourceMaps: true,
+          configFile: false,
+          babelrc: false,
+          sourceFileName: id,
         });
 
         if (result) {
           return {
-            code: result.code ?? '',
+            code: result.code || '',
             map: result.map,
           };
         }
