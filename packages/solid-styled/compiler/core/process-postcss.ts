3c9248f46bd14d3b18e122d177a4d878a7a6b059
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-28 22:20:25 +0800

    Remove `postcss` plugins

--- packages/solid-styled/compiler/core/process-postcss.ts
+++ packages/solid-styled/compiler/core/process-postcss.ts
@@ -1,7 +1,4 @@
 import postcss from 'postcss';
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
-import postcssNested from 'postcss-nested';
 import postcssSafeParser from 'postcss-safe-parser';
 import { StateContext } from '../types';
 
@@ -9,15 +6,7 @@ export default function processPostCSS(
   ctx: StateContext,
   content: string,
 ) {
-  const result = postcss([
-    cssnano({
-      plugins: [
-        autoprefixer,
-        postcssNested,
-        ...ctx.postcss.plugins,
-      ],
-    }),
-  ]);
+  const result = postcss(ctx.postcss.plugins);
   const processed = result.process(content, {
     from: ctx.ns,
     parser: postcssSafeParser,
